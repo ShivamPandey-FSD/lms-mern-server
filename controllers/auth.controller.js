ea6fb9efa01d508daee89da55b628a86229cb837
Shivam Pandey <shivampandey4597@gmail.com>
2026-01-15 17:28:21 +0530

    feat: add update password functionality

--- controllers/auth.controller.js
+++ controllers/auth.controller.js
@@ -9,130 +9,130 @@ const { generateForgotPasswordEmailTemplate } = require('../utils/emailTemplate'
 const crypto = require('crypto');
 
 const register = catchAsyncError(async (req, res, next) => {
- try {
-  const { name, email, password } = req.body || {};
+  try {
+    const { name, email, password } = req.body || {};
 
-  if (!name || !email || !password) {
-   return next(new ErrorHandler("Please fill all fields in the forms", 400));
+    if (!name || !email || !password) {
+      return next(new ErrorHandler("Please fill all fields in the forms", 400));
+    }
+
+    const isRegistered = await User.findOne({ email, accountVerified: true });
+
+    if (isRegistered) {
+      return next(new ErrorHandler("User already exists", 400));
+    }
+
+    const registrationAttemptsByUser = await User.find({
+      email,
+      accountVerified: false
+    });
+
+    if (registrationAttemptsByUser.length >= 5) {
+      return next(new ErrorHandler("You have exceeded the number of registration attempts. Please contact support", 400));
+    }
+
+    if (password.length < 8 || password.length > 16) {
+      return next(new ErrorHandler("Password length must be between 8 to 16 characters", 400));
+    }
+
+    const hashedPassword = await bcrypt.hash(password, 10);
+    const user = await User.create({
+      name,
+      email,
+      password: hashedPassword
+    });
+    const verificationCode = await user.generateVerificationCode();
+    await user.save();
+    sendVerificationCode(verificationCode, email, res);
+  } catch (error) {
+    next(error);
   }
-
-  const isRegistered = await User.findOne({ email, accountVerified: true });
-
-  if (isRegistered) {
-   return next(new ErrorHandler("User already exists", 400));
-  }
-
-  const registrationAttemptsByUser = await User.find({
-   email,
-   accountVerified: false
-  });
-
-  if (registrationAttemptsByUser.length >= 5) {
-   return next(new ErrorHandler("You have exceeded the number of registration attempts. Please contact support", 400));
-  }
-
-  if (password.length < 8 || password.length > 16) {
-   return next(new ErrorHandler("Password length must be between 8 to 16 characters", 400));
-  }
-
-  const hashedPassword = await bcrypt.hash(password, 10);
-  const user = await User.create({
-   name,
-   email,
-   password: hashedPassword
-  });
-  const verificationCode = await user.generateVerificationCode();
-  await user.save();
-  sendVerificationCode(verificationCode, email, res);
- } catch (error) {
-  next(error);
- }
 });
 
 const verifyOtp = catchAsyncError(async (req, res, next) => {
- try {
-  const { email, otp } = req.body || {};
+  try {
+    const { email, otp } = req.body || {};
 
-  if (!email || !otp) {
-   return next(new ErrorHandler("Email or OTP is missing", 400));
+    if (!email || !otp) {
+      return next(new ErrorHandler("Email or OTP is missing", 400));
+    }
+
+    const userAllEntries = await User.find({
+      email,
+      accountVerified: false
+    }).sort({ createdAt: -1 });
+
+    if (!userAllEntries) {
+      return next(new ErrorHandler("User not found", 404));
+    }
+
+    let user;
+
+    if (userAllEntries.length > 1) {
+      user = userAllEntries[0];
+      await User.deleteMany({
+        _id: { $ne: user._id },
+        email,
+        accountVerified: false
+      })
+    } else {
+      user = userAllEntries[0];
+    }
+
+    if (user.verificationCode !== Number(otp)) {
+      return next(new ErrorHandler("Invalid OTP", 400));
+    }
+
+    const currentTime = Date.now();
+    const verificatioCodeExpire = user.verificationCodeExpire.getTime();
+
+    if (currentTime > verificatioCodeExpire) {
+      return next(new ErrorHandler("OTP is expired. Please try again", 400));
+    }
+
+    user.accountVerified = true;
+    user.verificationCode = null;
+    user.verificationCodeExpire = null;
+    await user.save({ validateModifiedOnly: true });
+
+    sendToken(user, 200, "Account Verified", res);
+  } catch (error) {
+    return next(new ErrorHandler(`Internal Server Error: ${error}`, 500));
   }
-
-  const userAllEntries = await User.find({
-   email,
-   accountVerified: false
-  }).sort({ createdAt: -1 });
-
-  if (!userAllEntries) {
-   return next(new ErrorHandler("User not found", 404));
-  }
-
-  let user;
-
-  if (userAllEntries.length > 1) {
-   user = userAllEntries[0];
-   await User.deleteMany({
-    _id: { $ne: user._id },
-    email,
-    accountVerified: false
-   })
-  } else {
-   user = userAllEntries[0];
-  }
-
-  if (user.verificationCode !== Number(otp)) {
-   return next(new ErrorHandler("Invalid OTP", 400));
-  }
-
-  const currentTime = Date.now();
-  const verificatioCodeExpire = user.verificationCodeExpire.getTime();
-
-  if (currentTime > verificatioCodeExpire) {
-   return next(new ErrorHandler("OTP is expired. Please try again", 400));
-  }
-
-  user.accountVerified = true;
-  user.verificationCode = null;
-  user.verificationCodeExpire = null;
-  await user.save({ validateModifiedOnly: true });
-
-  sendToken(user, 200, "Account Verified", res);
- } catch (error) {
-  return next(new ErrorHandler(`Internal Server Error: ${error}`, 500));
- }
 })
 
 const login = catchAsyncError(async (req, res, next) => {
- const { email, password } = req.body || {};
+  const { email, password } = req.body || {};
 
- if (!email || !password) {
-  return next(new ErrorHandler("Please enter all the required fields", 400));
- }
+  if (!email || !password) {
+    return next(new ErrorHandler("Please enter all the required fields", 400));
+  }
 
- const user = await User.findOne({ email, accountVerified: true }).select("+password");
+  const user = await User.findOne({ email, accountVerified: true }).select("+password");
 
- if (!user) {
-  return next(new ErrorHandler("Invalid email or password", 400));
- }
+  if (!user) {
+    return next(new ErrorHandler("Invalid email or password", 400));
+  }
 
- const isPasswordMatched = await bcrypt.compare(password, user.password);
+  const isPasswordMatched = await bcrypt.compare(password, user.password);
 
- if (!isPasswordMatched) {
-  return next(new ErrorHandler("Please enter valid password", 400));
- }
- sendToken(user, 200, "Logged In Successfully", res);
+  if (!isPasswordMatched) {
+    return next(new ErrorHandler("Please enter valid password", 400));
+  }
+  sendToken(user, 200, "Logged In Successfully", res);
 });
 
 const logout = catchAsyncError(async (req, res, next) => {
- const user = await User.findById(req.user).select('+refreshToken');
+  const user = await User.findById(req.user).select('+refreshToken');
 
- user.refreshToken = null;
- await user.save({ validateBeforeSave: false });
+  user.refreshToken = null;
+  await user.save({ validateBeforeSave: false });
 
- res
-  .clearCookie('accessToken')
-  .clearCookie('refreshToken')
-  .status(200)
-  .json({ success: true, messsage: "Logged Out Successfully" });
+  res
+    .clearCookie('accessToken')
+    .clearCookie('refreshToken')
+    .status(200)
+    .json({ success: true, messsage: "Logged Out Successfully" });
 });
 
 const getUsers = catchAsyncError(async (req, res, next) => {
@@ -162,7 +162,7 @@ const forgotPassword = catchAsyncError(async (req, res, next) => {
 
   const resetPasswordUrl = `${process.env.FRONTEND_PORT}/password/reset/${resetToken}`;
   const message = generateForgotPasswordEmailTemplate(resetPasswordUrl);
-  
+
   try {
     await sendEmail({
       email: user.email,
@@ -187,7 +187,7 @@ const resetPassword = catchAsyncError(async (req, res, next) => {
 
   const user = await User.findOne({
     resetPasswordToken,
-    resetPasswordTokenExpire: { $gt: Date.now() }
+    resetPasswordExpire: { $gt: Date.now() }
   })
 
   if (!user) {
@@ -211,13 +211,47 @@ const resetPassword = catchAsyncError(async (req, res, next) => {
   sendToken(user, 200, "Password reset successfully", res);
 });
 
+const updatePassword = catchAsyncError(async (req, res, next) => {
+  const { currentPassword, newPassword, confirmPassword } = req.body;
+
+  if (!currentPassword || !newPassword || !confirmPassword) {
+    return next(new ErrorHandler("All fields are mandatory", 400));
+  }
+
+  const user = await User.findById(req.user).select("+password");
+
+  const isPasswordMatched = await bcrypt.compare(currentPassword, user.password);
+
+  if (!isPasswordMatched) {
+    return next(new ErrorHandler("Current password is invalid", 400));
+  }
+
+  if (newPassword.length < 8 || newPassword.length > 16) {
+    return next(new ErrorHandler("Password must be between 8 to 16 characters", 400));
+  }
+
+  if (newPassword !== confirmPassword) {
+    return next(new ErrorHandler("Password and confirm password do not match", 400));
+  }
+
+  const hashedPassword = await bcrypt.hash(newPassword, 10);
+  user.password = hashedPassword;
+  await user.save();
+  res.status(200).json({
+    success: true,
+    message: "Password updated successfully"
+  })
+
+});
+
 
 module.exports = {
- register,
- verifyOtp,
- login,
- logout,
- getUsers,
- forgotPassword,
- resetPassword
+  register,
+  verifyOtp,
+  login,
+  logout,
+  getUsers,
+  forgotPassword,
+  resetPassword,
+  updatePassword
 }
--- routes/auth.route.js
+++ routes/auth.route.js
@@ -1,5 +1,5 @@
 const express = require('express');
-const { register, verifyOtp, login, logout, getUsers, forgotPassword, resetPassword } = require('../controllers/auth.controller');
+const { register, verifyOtp, login, logout, getUsers, forgotPassword, resetPassword, updatePassword } = require('../controllers/auth.controller');
 const isAuthenticated = require('../middlewares/auth.middleware');
 
 const router = express.Router();
@@ -11,5 +11,6 @@ router.get('/logout', isAuthenticated, logout);
 router.get('/me', isAuthenticated, getUsers);
 router.post('/password/forgot', forgotPassword);
 router.patch('/password/reset/:token', resetPassword);
+router.patch('/password/update', isAuthenticated, updatePassword);
 
 module.exports = router;
